c9287fe7d5c212c6bad3fd80a892b84d54baab3f
ayyadh <hala.ayyad@mail.utoronto.ca>
2016-03-30 14:37:56 -0400

    added the shortcode to it

--- recipes.php
+++ recipes.php
@@ -1,8 +1,8 @@
 <?php
 /* 
-* Plugin Name: Recipes
+* Plugin Name: Gluten Free Recipes
 * Plugin URI: http://phoenix.sheridanc.on.ca/~ccit3443
-* Description: Creating and inserting new recipes reviews
+* Description: Custom post types for gluten free recipes 
 * Author: Hala Ayyad, Alaa Harb, Mohammed Hussein
 * Author URI: http://phoenix.sheridanc.on.ca/~ccit3443
 * Version: 1.0 
@@ -53,3 +53,32 @@ function glutenfree_register_post_type () {
 
 }
 add_action ('init', 'glutenfree_register_post_type');
+
+
+//shortcode for displaying gluten free posts
+//code was found on (http://code.tutsplus.com/tutorials/create-a-shortcode-to-list-posts-with-multiple-parameters--wp-32199) 
+//it was CHANGED to suit our needs.  
+add_shortcode( 'glutenfreesc', 'glutenfreesc' );
+function glutenfreesc( $atts ) {
+    ob_start();
+    $query = new WP_Query( array(
+        'post_type' => 'glutenfree',
+        'order' => 'ASC',
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+    ) );
+    if ( $query->have_posts() ) { ?>
+    <h2>Try the latest Gluten Free Recipe!</h2>
+        <ul class="clothes-listing">
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+            <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+            </li>
+            <?php endwhile;
+            wp_reset_postdata(); ?>
+        </ul>
+    <?php $myvariable = ob_get_clean();
+    return $myvariable;
+    }
+}
